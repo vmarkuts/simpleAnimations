1279940405c8a64ecb89792a7ef70c851f6a2ac3
vlad <vlad.markuts@gmail.com>
2020-04-12 04:52:22 +0300

    working animation

--- script.js
+++ script.js
@@ -1,75 +1,86 @@
 var btnRound = $('#round');
 var btnShadow = $('#shadow');
 var btnRenault = $('#renault');
-var zelenskiyImage = $('img');
-var zelenskiyMidImage = $('#middle');
 var btnWave = $('#wave');
 var shadow = false;
 var count = 1;
-//условие ? выражение1 : выражение2
+var timeCounter = 0;
+var imgLink = '<img src="https://image.ua/wp-content/uploads/ae88b37a1f9083d2339426b0eb1057de_1579709854-554x466.jpg">';
+init(3);
+var firstZe, midZe, lastZe;
+updatePositions();
 
 btnRound.click(function () {
-	if (!zelenskiyImage.hasClass('zelenskiyRound')) {
-		
+	if (!images.hasClass('zelenskiyRound')) {
 		btnRound.text('Make not round');
-		zelenskiyImage.addClass('zelenskiyRound');
+		images.addClass('zelenskiyRound');
 	} else {
-		zelenskiyImage.removeClass('zelenskiyRound');
+		images.removeClass('zelenskiyRound');
 		btnRound.text('Round Zelenskiy');
 	}
 });
 
 btnShadow.click(function(){
 	shadow ? $(this).text('Add Some Shadow') : $(this).text('Remove Shadow');
-	zelenskiyImage.toggleClass('zelenskiyShadow');
+	images.toggleClass('zelenskiyShadow');
 	shadow = !shadow;
 });
 
-//addEventListener() = on() in jQuery
-
 btnRenault.click(function(){
 	if (count === 1) {
-		zeUp(zelenskiyMidImage);
+		zeUp(midZe);
 		return count = 2;
 	}
 	if (count === 2) {
-		zeDown(zelenskiyMidImage);
-		zeUp('img:first-of-type');
-		setTimeout(function(){zeUp('img:last-of-type');}, 200);
+		zeDown(midZe);
+		removeStyle(midZe);
+		zeUp(firstZe);
+		setTimeout(function(){zeUp(lastZe);}, 200);
 		return count = 3;
 	} 
 	if (count === 3) {
-		zeDown('img:first-of-type');
-		zeDown('img:last-of-type');
+		zeDown(firstZe);
+		removeStyle(firstZe)
+		zeDown(lastZe);
+		removeStyle(lastZe);
 		return count = 1;
 	}
 });
+
+function removeStyle (image) {
+	image.attr('style','');
+}
 
 btnWave.click(function(){
 	if (count !== 1) {
 		alert('put zelenskiy on earth firstly');
 	} else {
+		timeCounter = 0;
 		zeWaveUp();
 		zeWaveDown();
+		for (var i = 0; i < images.length; i++) {
+			setTimeout(function(){removeStyle(images);},timeCounter)
+		}
+		timeCounter = 0;
 	}
 });
 
 function zeWaveUp() {
-	for (let i = 0, j = 0; i < $('img').length; i++ , j+=200) {
-		setTimeout(function(){zeUp($('img')[i]);}, j);
+	for (let i = 0; i < $(images).length; i++ , timeCounter+=200) {
+		setTimeout(function(){zeUp($(images)[i]);}, timeCounter);
 	}
 }
 
 function zeWaveDown() {
-	for (let i = 0, j = 500; i < $('img').length; i++ , j+=200) {
-		setTimeout(function(){zeDown($('img')[i]);}, j);
+	for (let i = 0; i < $(images).length; i++ , timeCounter+=200) {
+		setTimeout(function(){zeDown($(images)[i]);}, timeCounter);
 	}
 }
 
 function zeUp(image) {
 	$(image).css({
 		transform: 'scaleX(-1)',
-		-webkit-transform: 'scaleX(-1)',
+		webkitTransform: 'scaleX(-1)',
 		width: '300px'
 	});
 }
@@ -77,14 +88,14 @@ function zeUp(image) {
 function zeDown (image) {
 	$(image).css({
 		transform: 'scaleX(+1)',
-		-webkit-transform: 'scaleX(+1)',
+		webkitTransform: 'scaleX(+1)',
 		width: '250px'
 	});
 }
 
 $('button').click(function(){
 	var text = $(this).text();
-	console.log('u clicked ' + text);
+	// console.log('u clicked ' + text);
 });
 
 $('input[type="text"]').keypress(function(event){
@@ -101,19 +112,48 @@ $('input').on('mouseenter',function(){
     console.log('mouse enter!');
 });
 
-$('img').on('mouseover',function(){
-    $(this).addClass('zeAnimation');
+function init(num) {
+	for (var i = 0; i < num; i++) {
+		addZe();
+	}
+}
+
+function addZe () {
+	$('#zeImgArray').append(imgLink);
+	updatePositions();
+}
+
+function removeZe() {
+	if (images.length !== 1) {
+		$('#zeImgArray img:last-of-type').remove();
+		updatePositions();
+	} else {
+		alert('You cannot delete the last of the best President');
+	}
+}
+
+$('#addImage').click(function(){
+	addZe();
 });
 
-
-$('img').on('mouseout',function(){
-    $(this).removeClass('zeAnimation');
+$('#removeImage').click(function(){
+	removeZe();
 });
 
+function updatePositions () {
+	var str = '';
+	images = $('#zeImgArray img');
+	firstZe = $('#zeImgArray img:first-of-type');
+	var X = Math.round(images.length / 2);
+	str = '#zeImgArray img:eq(-' + X + ')';
+	midZe = $(str);
+	lastZe = $('#zeImgArray img:last-of-type');
+}
 
+images.on('mouseover',function(){
+	$(this).addClass('zeAnimation');
+});
 
-// $('input[type="text"]').keypress(function(){
-// 	console.log('u pressed the key');
-// });
-
-// zelenskiyImage.on().attr('src','https://cdn.riastatic.com/photosnewr/ria/news_common/avtomobil-nedeli-renault-k-ze__246923-1680x0.jpg');
+images.on('mouseout',function(){
+	$(this).removeClass('zeAnimation');
+});
